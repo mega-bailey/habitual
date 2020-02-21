87ac60006e712ba59664e1f7b8eb7554c4faac81
Megan Bailey <megan-bailey@outlook.com>
2020-02-21 13:55:44 -0800

    JS complete and Heroku set up

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # habitual
 
-An app to help track habits - using HTML, SASS, Vanilla JS, and JQuery
+An app to help track habits - using HTML, SASS, Vanilla JS, and JQuery.
+
+Would like to refactor to have data persistence with localStorage.
--- app.js
+++ app.js
@@ -1,16 +1,11 @@
-// var weekdayEl = document.querySelector('.weekdays');
-// const parentDiv = document.getElementById('parentDiv');
-// const newHabitInput = document.querySelector('.new-habit__input-box');
-
-//GET INPUT VALUE
+//GET NEW HABIT INPUT VALUE
 $('input:text').keypress(function(e) {
   let newHabit = $(this).val();
   if (e.which == 13 && newHabit != '') {
-    // $(this).blur();
     e.currentTarget.value = '';
     e.preventDefault();
     //ADD THIS NEW HABIT ROW BASED ON INPUT
-    var newHabitRow = `<div class="habit-row"><div class="habit-title"><h2 class="habit-title__title">${newHabit}</h2><span class="btn-delete">&times;</span></div><div class="habits"><div class="habits__container"><div class="habits__box glow ">&#9673;</div><div class="habits__box glow ">&#9673;</div><div class="habits__box glow ">&#9673;</div><div class="habits__box glow ">&#9673;</div><div class="habits__box glow ">&#9673;</div><div class="habits__box glow ">&#9673;</div><div class="habits__box glow ">&#9673;</div></div></div></div>`;
+    var newHabitRow = `<div class="habit-row"><div class="habit-title"><h2 class="habit-title__title">${newHabit}</h2><span class="btn-delete">&times;</span></div><div class="habits"><div class="habits__container"><div class="habits__box glow blank">&#9673;</div><div class="habits__box glow blank ">&#9673;</div><div class="habits__box glow blank ">&#9673;</div><div class="habits__box glow blank ">&#9673;</div><div class="habits__box glow blank ">&#9673;</div><div class="habits__box glow blank ">&#9673;</div><div class="habits__box glow blank ">&#9673;</div></div></div></div>`;
     $(newHabitRow).insertAfter('.weekdays');
     //DELETE THE HABIT WHEN X IS CLICKED
     $('span').click(function(e) {
@@ -23,22 +18,24 @@ $('input:text').keypress(function(e) {
       // e.stopPropagation();
     });
     //TOGGLE BETWEEN COLORS
-
     $('.habits__box').click(function(e) {
-      var classes = ['yes', 'no', 'skip', 'blank'];
-      var i = classes.length;
-      while (i--) {
-        $('.habits-box').toggleClass(classes[i]);
+      if ($(this).hasClass('blank')) {
+        $(this)
+          .removeClass('blank')
+          .addClass('yes');
+      } else if ($(this).hasClass('yes')) {
+        $(this)
+          .removeClass('yes')
+          .addClass('no');
+      } else if ($(this).hasClass('no')) {
+        $(this)
+          .removeClass('no')
+          .addClass('skip');
+      } else if ($(this).hasClass('skip')) {
+        $(this)
+          .removeClass('skip')
+          .addClass('blank');
       }
     });
   }
 });
-
-//sort of works, http://jsfiddle.net/KdfEV/, but would need to seriously alter css
-// $('.yes, .no, .skip').click(function() {
-//   this.className = {
-//     three: 'yes',
-//     one: 'no',
-//     two: 'skip'
-//   }[this.className];
-// });
