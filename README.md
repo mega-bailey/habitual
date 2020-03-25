2b863f4fab458c759176c4dfbab79b24c0119dd5
Megan Bailey <megan-bailey@outlook.com>
2020-03-25 08:29:31 -0700

    toggle func correct

--- README.md
+++ README.md
@@ -3,5 +3,3 @@
 <h1><a href="https://habitual-app-mlb.herokuapp.com/" target="_blank">Habitual App</a></h1>
 
 An app to help track habits - using HTML, SCSS, JS, and JQuery.
-
-Planning to include data persistence with localStorage.
--- app.js
+++ app.js
@@ -4,9 +4,13 @@ $('input:text').keypress(function(e) {
   if (e.which == 13 && newHabit != '') {
     e.currentTarget.value = '';
     e.preventDefault();
+
     //ADD THIS NEW HABIT ROW BASED ON INPUT
-    var newHabitRow = `<div class="habit-row"><div class="habit-title"><h2 class="habit-title__title">${newHabit}</h2><span class="btn-delete">&times;</span></div><div class="habits"><div class="habits__container"><div class="habits__box glow blank">&#9673;</div><div class="habits__box glow blank ">&#9673;</div><div class="habits__box glow blank ">&#9673;</div><div class="habits__box glow blank ">&#9673;</div><div class="habits__box glow blank ">&#9673;</div><div class="habits__box glow blank ">&#9673;</div><div class="habits__box glow blank ">&#9673;</div></div></div></div>`;
-    $(newHabitRow).insertAfter('.weekdays');
+    var $newHabitRow = $(
+      `<div class="habit-row"><div class="habit-title"><h2 class="habit-title__title">${newHabit}</h2><span class="btn-delete">&times;</span></div><div class="habits"><div class="habits__container"><div class="habits__box glow blank">&#9673;</div><div class="habits__box glow blank ">&#9673;</div><div class="habits__box glow blank ">&#9673;</div><div class="habits__box glow blank ">&#9673;</div><div class="habits__box glow blank ">&#9673;</div><div class="habits__box glow blank ">&#9673;</div><div class="habits__box glow blank ">&#9673;</div></div></div></div>`
+    );
+    $newHabitRow.insertAfter('.weekdays');
+
     //DELETE THE HABIT WHEN X IS CLICKED
     $('span').click(function(e) {
       $(this)
@@ -15,27 +19,27 @@ $('input:text').keypress(function(e) {
         .fadeOut(400, function() {
           $(this).remove();
         });
-      // e.stopPropagation();
     });
-    //TOGGLE BETWEEN COLORS
-    $('.habits__box').click(function(e) {
-      if ($(this).hasClass('blank')) {
-        $(this)
-          .removeClass('blank')
-          .addClass('yes');
-      } else if ($(this).hasClass('yes')) {
-        $(this)
-          .removeClass('yes')
-          .addClass('no');
-      } else if ($(this).hasClass('no')) {
-        $(this)
-          .removeClass('no')
-          .addClass('skip');
-      } else if ($(this).hasClass('skip')) {
-        $(this)
-          .removeClass('skip')
-          .addClass('blank');
-      }
-    });
+  }
+});
+
+//TOGGLE BETWEEN COLORS
+$('.container').click(function(e) {
+  if ($(e.target).hasClass('blank')) {
+    $(e.target)
+      .removeClass('blank')
+      .addClass('yes');
+  } else if ($(e.target).hasClass('yes')) {
+    $(e.target)
+      .removeClass('yes')
+      .addClass('no');
+  } else if ($(e.target).hasClass('no')) {
+    $(e.target)
+      .removeClass('no')
+      .addClass('skip');
+  } else if ($(e.target).hasClass('skip')) {
+    $(e.target)
+      .removeClass('skip')
+      .addClass('blank');
   }
 });
